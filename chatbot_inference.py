a355d2c28faf4dbce5129f34c2a4ceb04635fc42
KaitaoQiu <kaitaoqiu.cs@gmail.com>
2024-04-04 14:37:03 -0700

    Add google search for bot.

--- chatbot_inference.py
+++ chatbot_inference.py
@@ -2,10 +2,11 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from peft import PeftModel
 import os
-
+import json
 def load_model():
     base_model = 'NousResearch/Llama-2-7b-chat-hf'
     new_model_path = "./security_miles_model/ppo_model"
+    # new_model_path = "./security_miles_model/ppo_model"
 
     load_model = AutoModelForCausalLM.from_pretrained(
         base_model,
@@ -25,7 +26,7 @@ def load_model():
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "right"
 
-    pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
+    pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=1024)
     return pipe
 
 os.environ['HF_HOME'] = './cache'
@@ -33,14 +34,40 @@ print("huggingface cache is in {}".format(os.getenv('HF_HOME')))
 
 
 def generate_answer(question, pipeline):
-    prompt = f"<s>[INST] {question} [/INST]"
 
+    import http.client
+    import json
+
+    conn = http.client.HTTPSConnection("google.serper.dev")
+    payload = json.dumps({
+    "q": question
+    })
+    config_path = "./model_config/google_search.json"
+    with open(config_path) as f:
+        api_config = json.load(f)
+    headers = {
+    'X-API-KEY': api_config["SERPER_API_KEY"],
+    'Content-Type': 'application/json'
+    }
+    conn.request("POST", "/search", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+    meta_data = json.loads(data.decode("utf-8"))
+    background_string  = ""
+    if 'knowledgeGraph' in meta_data:
+        background_string += str(meta_data['knowledgeGraph'])
+    elif "answerBox" in meta_data:
+        background_string += str(meta_data["answerBox"])
+    else:
+        background_string += str(meta_data["organic"][0])
+    prompt = f"<s>[INST] {question}, also I will give you some knowledgegraph:{background_string} [/INST]"
     result = pipeline(prompt)
-
     return result[0]['generated_text'].replace('<s>', '').replace('</s>', '')
+
 
 if __name__ == "__main__":
     import sys
     pipeline = load_model()
-    question = sys.argv[1] if len(sys.argv) > 1 else "What's cybersecurity?"
+    question = sys.argv[1] if len(sys.argv) > 1 else "What's result of 1+1?"
     print(generate_answer(question, pipeline))
